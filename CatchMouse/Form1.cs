da8070038dc2f96e7960ea13b9667804a0dbd489
NeroComp <NeroComp@DESKTOP-7HUNHTF>
2022-05-22 21:32:08 +0400

    Add project files.

---
+++ CatchMouse/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CatchMouse
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_MouseHover(object sender, EventArgs e)
+        {
+            if(sender is PictureBox pcb)
+            {
+                Random random = new Random();
+                int randomX = random.Next(this.Size.Width - 100);
+                int randomY = random.Next(this.Size.Height - 100);
+                pcb.Location = new Point(randomX, randomY);
+                
+            }
+        }
+       
+    }
+}
